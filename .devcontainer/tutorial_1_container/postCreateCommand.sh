1b1fa8e6e3d770d173a4aa20b102750286ded8a2
Tyler Erickson <tyler@vorgeo.com>
2025-03-07 12:04:54 -0800

    Move postCreateCommands to separate file (tutorial 1)

---
+++ .devcontainer/tutorial_1_container/postCreateCommand.sh
@@ -0,0 +1,11 @@
+# Install pixi
+curl -fsSL https://pixi.sh/install.sh | bash
+
+# Install conda and pypi packages with pixi
+/home/vscode/.pixi/bin/pixi install
+
+# # Display information on the pixi environment
+# /home/vscode/.pixi/bin/pixi info
+
+# Install a Jupyter kernel for the devcontainer
+/home/vscode/.pixi/bin/pixi run python -m ipykernel install --user --name=devcontainer --display-name 'Python (Dev Container)'
